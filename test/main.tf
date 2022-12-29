ec3e6d0756f5f023eeb2c26da01395bf5cf9eb87
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-29 15:52:41 -0500

    fix: Update test
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- test/main.tf
+++ test/main.tf
@@ -1,9 +1,10 @@
 module "backup_config" {
-  source                 = "../../terraform-azurerm-azure_backup"
-  create_resource_group  = true
-  resource_group_name    = "test-rg"
-  location_name          = "eastus"
-  name = "recovery-vault-test"
+  source                = "../../terraform-azurerm-azure_backup"
+  create_resource_group = true
+  resource_group_name   = "test-rg"
+  location_name         = "eastus"
+  name                  = "recovery-vault-test"
+  sku_name              = "Standard"
 
   tags = {
     "test" = "test"
