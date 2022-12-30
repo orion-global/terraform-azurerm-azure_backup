26b261edb7cae0df8d82ba35a4403e633cd21984
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-29 19:36:58 -0500

    feat: daily ackup
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_backup_policy_vm" "backup_policy" {
   name                = each.key
   resource_group_name = var.resource_group_name
   recovery_vault_name = azurerm_recovery_services_vault.recovery_vault.name
-  timezone            = each.value.timezone
+  timezone            = each.value[timezone]
   policy_type         = each.value.policy_type
 
   # instant_restore_retention_days - (Optional) Specifies the instant restore retention range in days. Possible values are between 1 and 5 when policy_type is V1, and 1 to 30 when policy_type is V2.
@@ -73,20 +73,20 @@ resource "azurerm_backup_policy_vm" "backup_policy" {
     time      = each.value.time
   }
 
-  dynamic "retention_daily" {
-    for_each = (each.value.frequency == "Dayli" ? var.backup_policy["${each.key}"] : [])
-    content {
-      count = each.value.retention_days
-    }
-  }
+  # dynamic "retention_daily" {
+  #   for_each = (each.value.frequency == "Dayli" ? var.backup_policy["${each.key}"] : [])
+  #   content {
+  #     count = each.value.retention_days
+  #   }
+  # }
 
   # retention_daily - (Optional) Configures the policy daily retention as documented in the retention_daily block below. Required when backup frequency is Daily.
   #   count - (Required) The number of daily backups to keep. Must be between 7 and 9999.
   #   ~> Note: Azure previously allows this field to be set to a minimum of 1 (day) - but for new resources/to update this value on existing Backup Policies - this value must now be at least 7 (days).
 
-  # retention_daily {
-  #   count = each.value.retention_days
-  # }
+  retention_daily {
+    count = each.value.retention_days
+  }
 
   # retention_weekly - (Optional) Configures the policy weekly retention as documented in the retention_weekly block below. Required when backup frequency is Weekly.
   #   count - (Required) The number of weekly backups to keep. Must be between 1 and 9999
